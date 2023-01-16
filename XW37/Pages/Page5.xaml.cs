f0a31c3f4f8ca46fbae7b36cd6bfb0f59b19d7be
enesayazz <enesayaz_53@outlook.com>
2023-01-16 16:00:00 +0300

    final

--- XW37/Pages/Page5.xaml.cs
+++ XW37/Pages/Page5.xaml.cs
@@ -12,4 +12,12 @@ public partial class Page5 : ContentPage
     {
         Assignment_List_Wiev.ItemsSource = App.DBTrans.GetAssignments();
     }
+
+    private void Button_Delete_Clicked(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        App.DBTrans.DeleteAssignment((int)button.BindingContext);
+        Assignment_List_Wiev.ItemsSource = App.DBTrans.GetAssignments();
+
+    }
 }
